9a5f8efe81814f4828627152ec64d42866d3cb67
S1lv3rn <crisada99@hotmail.com>
2019-03-02 13:14:26 +0000

    stuff

--- src/Pong_frame.java
+++ src/Pong_frame.java
@@ -3,14 +3,19 @@ import javax.swing.*;
 
 public class Pong_frame {
 
-    public void createFrame(){
+    public void createFrame() {
 
         JFrame pongFrame = new JFrame("Pong");
-        pongFrame.setSize(2048,2048);
+        pongFrame.setSize(2048, 2048);
         pongFrame.setVisible(true);
         pongFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
     }
+
+
+
+
+}
 
 
 
