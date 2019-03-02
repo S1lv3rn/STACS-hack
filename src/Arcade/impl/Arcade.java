ec115846c846457668d4d47c607b392dbed25292
Darsh Manoj <vishnu9145@hotmail.com>
2019-03-02 12:42:23 +0000

    basic implementation

--- src/Arcade/impl/Arcade.java
+++ src/Arcade/impl/Arcade.java
@@ -1,12 +1,35 @@
 package Arcade.impl;
-
 public class Arcade {
-
+static String game[] = new String[1];
     public static void main(String[] args) {
-
-
-        if ()
+        game[0] = "Pong";
+        String gameenter = new String();
+        if (args.length != game.length)
+        {
+            System.out.println("Select game:");
+            int count = 0;
+            for(String gamelist:game)
+            {
+                System.out.println (++count + "." + gamelist);
+            }
+            System.out.println (gameenter);
+            for (int i = 0; i< game.length; i++)
+            {
+                if (game[i].equals(gameenter))
+                {
+                    // execute the related game
+                }
+            }
+        }
+        else
+        {
+            for (int i = 0; i< game.length; i++)
+            {
+                if (game[i].equals(gameenter))
+                {
+                    // execute the related game
+                }
+            }
+        }
     }
-
-
 }
